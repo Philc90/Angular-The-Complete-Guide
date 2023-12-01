59618a91753ba0f7ed501a24b78107633c9e6944
Philip Chiang <philipchiang5@gmail.com>
2023-11-30 20:07:01 -0800

    Ch 13 end - Subject instead of EventEmitter

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from './service/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  private activatedSub: Subscription;
+  
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(didAactivate => {
+      this.userActivated = didAactivate
+    })
+  }
+
+  ngOnDestroy() {
+    this.activatedSub.unsubscribe()
+  }
 }
