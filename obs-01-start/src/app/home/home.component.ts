9c4199b28c3d6b480208d923bcc82f33667a719f
Philip Chiang <philipchiang5@gmail.com>
2023-12-07 22:25:22 -0800

    more examples

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs';
+import { Subject, Subscription, from, fromEvent, interval, pipe } from 'rxjs';
 import { Observable } from 'rxjs-compat';
-import { filter, map } from 'rxjs/operators';
+import { filter, map, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -15,37 +15,61 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
+
+    // map operator example
+    // const clicks = fromEvent<PointerEvent>(document, 'click');
+    // const positions = clicks.pipe(map(ev => ev.clientX));
+
+    // positions.subscribe(x => console.log(x));
+
+    // takeUntil operator example
+    // const source = interval(1000);
+    // const clicks = fromEvent(document, 'click');
+    // const result = source.pipe(takeUntil(clicks));
+    // result.subscribe(x => console.log(x), err => console.log(err), () => console.log("done"));
+
+
+    // create observable from array example
+    // from([1, 2, 3]).pipe(
+    //   map(x => x * 2)
+    // ).subscribe(
+    //   num => console.log('from array:',num)
+    // )
+
+    // example: interval
     // this.firstObsSubscription = interval(1000).subscribe(count => {
     //   console.log(count);
     // })
-    const customIntervalObservable = Observable.create((observer) => {
-      let count = 0;
-      setInterval(() => {
-        observer.next(count)
-        if (count === 2) {
-          observer.complete()
-        }
-        if (count > 3) {
-          observer.error(new Error('count > 3'))
-        }
-        count++
-      },1000)
-    })
+    // creating interval from scratch
+    // const customIntervalObservable = new Observable((observer) => {
+    //   let count = 0;
+    //   setInterval(() => {
+    //     observer.next(count)
+    //     if (count === 2) {
+    //       observer.complete()
+    //     }
+    //     if (count > 3) {
+    //       observer.error(new Error('count > 3'))
+    //     }
+    //     count++
+    //   },1000)
+    // })
 
-    this.firstObsSubscription = customIntervalObservable.pipe(filter((data: number) => {
-      return data > 0;
-    }),
-    map((data:number) => {
-      return 'Round: ' + (data+1)
-    })).subscribe(data => {
-      console.log(data)
-    }, error => {
-      // observervable ends: cancelled due to error
-      console.error(error)
-    }, complete => {
-      // observervable ends: completed
-      console.log('COMPLETE')
-    })
+    // this.firstObsSubscription = customIntervalObservable.pipe(filter((data: number) => {
+    //   return data > 0;
+    // }),
+    // map((data:number) => {
+    //   return 'Round: ' + (data+1)
+    // })).subscribe(data => {
+    //   console.log(data)
+    // }, error => {
+    //   // observervable ends: cancelled due to error
+    //   console.error(error)
+    // }, () => {
+    //   // observervable ends: completed
+    //   console.log('COMPLETE')
+    // })
+
 
     // ALTERNATIVE syntax: pass in observable object
     // .subscribe({
@@ -61,6 +85,30 @@ export class HomeComponent implements OnInit, OnDestroy {
     //     console.log('COMPLETE')
     //   }
     // })
+
+
+    
+    // https://www.youtube.com/watch?v=_iXd0SWr3Sw
+
+    // const cold = new Observable((observer) => {
+    //   observer.next(Math.random() * 10)
+    // })
+
+    // cold.subscribe(res => { console.log('cold:', res) })
+
+    // cold.subscribe(res => { console.log('cold:', res) })
+
+    // const hot = new Subject();
+
+    // hot.subscribe(res => {
+    //   console.log('hot:', res)
+    // })
+
+    // hot.subscribe(res => {
+    //   console.log('hot:', res)
+    // })
+
+    // hot.next(Math.round(Math.random() * 10))
   }
 
   ngOnDestroy(): void {
